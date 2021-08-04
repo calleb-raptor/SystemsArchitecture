cc4bf7bfedd0d0a38b68ced15f7f05c22517da46
calleb-raptor <carlbrand1995@me.com>
2021-08-04 22:18:38 +0200

    changed If statements to switch cases and split out to more functions...Issue with using scanf as expression in the switchstatement - to be looked at, however the current one works by doing scanf outside of statement and calling var in statment

--- manager.c
+++ manager.c
@@ -3,12 +3,19 @@
 void newAccount();
 int exitManu();
 void accountList();
+void invalidInput();
+void accountNumberFlow();
+
+void invalidInput()
+{
+    printf("Invalid input...\n");
+}
 
 void menu()
 {
     printf("Hello!\nPlease select one of the following options by typing the number and enter:\n1. New Account\n2. List Accounts\n3. Exit\n");
     int option;
-    scanf("%i", &option);
+    // scanf("%i", &option);
 
     switch (scanf("%i", &option))
     {
@@ -42,41 +49,47 @@ void newAccount()
     char name[20];
     char agreement;
     printf("New Account!\nPlease enter the name of the account:\n");
-    scanf(" %s", name);
+    scanf("%s", name);
     printf("You entered: %s\nIs this correct?\n", name);
     printf("[Y]    [N]\n");
     scanf(" %c", &agreement);
-    if (agreement == 'Y')
+    switch (agreement)
     {
-        int accountNumber;
-        // char ack;
-        printf("Great, now please enter your account number:\n");
-        scanf(" %i", &accountNumber);
-        printf("You entered: %i\nIs this correct?\n", accountNumber);
-        printf("[Y]      [N]\n");
-        scanf(" %c", &agreement);
-        // change this to a swich case
-        if (agreement == 'Y')
-        {
-            printf("Account set up with below details:\nAccount Name: %s\nAccount Number: %i\n", name, accountNumber);
-        }
-        else if (agreement == 'N')
-        {
-            printf("Okay... Would you like to change the details of the account?\nPlease restart the program...");
-        }
-        else
-        {
-            printf("Invalid input...");
-        }
+    case 'y':
+    case 'Y':
+        accountNumberFlow(name);
+        break;
+    case 'n':
+    case 'N':
+        exitManu();
+        break;
+    default:
+        invalidInput();
     }
-    else if (agreement == 'N')
+}
+
+void accountNumberFlow(char *name)
+{
+    int accountNumber;
+    char agreement;
+    printf("Great, now please enter your account number:\n");
+    scanf(" %i", &accountNumber);
+    printf("You entered: %i\nIs this correct?\n", accountNumber);
+    printf("[Y]      [N]\n");
+    scanf(" %c", &agreement);
+
+    switch (agreement)
     {
-        printf("Exiting...");
-    }
-    // create a function in and of itself for invalid input...makes the code more reusable
-    else
-    {
-        printf("Invalid input...");
+    case 'y':
+    case 'Y':
+        printf("Account set up with below details:\nAccount Name: %s\nAccount Number: %i\n", name, accountNumber);
+        break;
+    case 'n':
+    case 'N':
+        printf("Okay... Would you like to change the details of the account?\nPlease restart the program...");
+        break;
+    default:
+        invalidInput();
     }
 }
 
